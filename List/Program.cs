257efc189986aa4c79154f346a759f70789db2c4
farhullin2017@gmail.com <90451035+IlnazFarkhullin@users.noreply.github.com>
2024-01-24 11:48:38 +0300

    list

---
+++ List/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> name = new List<string>() //объявляем список с начальными значениями
+                                                  //в скобках можно указать ёмкость списка
+            {
+                 "Ильдина",
+                 "Влад",
+                 "Софа",
+                 "Полина",
+                 "Юра",
+                 "Эдуард",
+                 "Дима",
+                 "Маша"
+            };
+            Console.WriteLine("Вывод на консоль:");
+            foreach (string s in name) //перебираем список
+            {
+                Console.WriteLine(s); //выводим на консоль
+            }
+            Console.WriteLine();
+
+            //Добавление
+            name.Add("Лариса"); //добавление значения
+            name.AddRange(new[] { "Алина", "Марина", "Богдан", "Миша" }); //добавление массива
+
+            // также можно было бы добавить другой список
+            // name.AddRange(new List<string>(){ "Ирина", "Гриша", "Вика", "Женя"});
+            Console.WriteLine("Добавление новых элементов:");
+            foreach (string s in name) //перебираем список
+            {
+                Console.WriteLine(s); //выводим на консоль
+            }
+            Console.WriteLine();
+
+            //Добавление элемента на определённую позицию
+            name.Insert(3, "Ваня"); // указывается позиция и значение
+
+            //также можно добавть массив на определённую позицию
+            //name.InsertRange(1, new string[] { "Тимур", "Даша" }); // вставляем массив с индекса 1
+            Console.WriteLine("Добавление на определённую позицию:");
+            foreach (string s in name) //перебираем список
+            {
+                Console.WriteLine(s); //выводим на консоль
+            }
+            Console.WriteLine();
+
+            //Удаление
+            //Удаляем элемент по индексу
+            string del = name[8]; //выбираем определённый элемент, для того чтобы потом вывести его на консоль
+            name.RemoveAt(8);//удаляем по индексу
+            Console.WriteLine("Удаление элементов из списка:");
+            foreach (var item in name)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Удалённое значение под индексом 8 = {del}");
+
+            Console.WriteLine();
+            //Удаляем элемент по значению
+            Console.WriteLine("Удаляем по значению:");
+            name.Remove("Влад");
+            foreach (var item in name)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ List_2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Заполнить List случайными числами.
+            //Затем пользователь вводит случайное число, определить есть ли элемент с таким индексом и вывести его на консоль, затем удалить данный элемент
+
+            List<int> list = new List<int>();
+            Random rand = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(rand.Next(1, 100));
+            }
+
+            Console.WriteLine("Исходный список:");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Введите индекс:");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            if (index >= 0 && index < list.Count)
+            {
+                Console.WriteLine($"Элемент с индексом {index} равен: {list[index]}");
+                list.RemoveAt(index);
+                Console.WriteLine($"Элемент {list[index]} удален");
+                Console.WriteLine();
+                Console.WriteLine("Список после удаления:");
+
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Элемент с таким индексом не найден");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
